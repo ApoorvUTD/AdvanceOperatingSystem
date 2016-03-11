9ff4e8707c8d389d75096ed161901bc0782a7dd4
RamHariesh <RamHariesh@RamPC>
2016-03-11 00:06:05 -0600

    Maekawa Changes

--- MaekawaAlgorithm/src/Maekawa.java
+++ MaekawaAlgorithm/src/Maekawa.java
@@ -3,12 +3,12 @@ public class Maekawa {
 
 	//blocking queue
 	public static void csEnter(){
-		
+		//enter in to critical section
 		
 	}
 	
 public static void csLeave(){
-		
+		//leave the critical section
 		
 	}
 }
