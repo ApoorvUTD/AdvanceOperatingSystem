5568aae04e5453332ec5f8bd834d135ef6811966
Sarat Chandra <sarat.vrns@gmail.com>
2016-03-11 00:21:27 -0600

    testing ssh key
    Change-Id: Ifce9f4a403176434308c1606335df1a4f059b867

--- MaekawaAlgorithm/src/Maekawa.java
+++ MaekawaAlgorithm/src/Maekawa.java
@@ -5,7 +5,11 @@ public class Maekawa {
 	Node node;
 	public static void csEnter(int nodeID){
 		//enter in to critical section
+<<<<<<< Upstream, based on origin/master
 
+=======
+		//foo
+>>>>>>> 0b79278 testing ssh key
 	}
 	
 	public static void csLeave(){
