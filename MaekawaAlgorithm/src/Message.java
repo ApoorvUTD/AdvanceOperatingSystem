7705568db7a37ce3382271aef16fa7e558b3ac34
Apoorv Kumar <axk132231@utdallas.edu>
2016-03-10 23:49:45 -0600

    initializing node
    Change-Id: I37fb03b28657adc0981a4f83f50646c455465289

--- MaekawaAlgorithm/src/Message.java
+++ MaekawaAlgorithm/src/Message.java
@@ -1,4 +1,50 @@
-
 public class Message {
+	
+	public enum MessageType
+	{
+		REQUEST,RELEASE,INQUIRE,FAIL,LOCK;
+	}
+	
+	int sourceId;
+	int destId;
+	int quorum;
+	MessageType msgType;
+	
+	
+	
+	
+	public int getSourceId() {
+		return sourceId;
+	}
+	public void setSourceId(int sourceId) {
+		this.sourceId = sourceId;
+	}
+	public int getDestId() {
+		return destId;
+	}
+	public void setDestId(int destId) {
+		this.destId = destId;
+	}
+	
+	public MessageType getMsgType() {
+		return msgType;
+	}
+	public void setMsgType(MessageType msgType) {
+		this.msgType = msgType;
+	}
+	
+
+	public static void sendMessage(Message message) {
+
+		//Main.nodeArray[message.getDestId()].getQueue().add(message);
+	}
+	@Override
+	public String toString() {
+		return "Messege [sourceId=" + sourceId + ", destId=" + destId
+				+ ", distFromRoot="  + ", msgType=" + msgType
+				+ "]";
+	}
+
+
 
 }
