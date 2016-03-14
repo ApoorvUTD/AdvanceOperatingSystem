a5bebb94b842952fb57623906fd9b6bde8105424
Sarat Chandra <sarat.vrns@gmail.com>
2016-03-14 03:27:53 -0500

    adding cleanup.sh launcher.sh
    Change-Id: I34c795cc37f263477925d64debe3ed963063b4d2

--- MaekawaAlgorithm/src/foo.java
+++ MaekawaAlgorithm/src/foo.java
@@ -4,7 +4,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 public class foo {
-    public static void main() throws Exception{
+    public static void main(String args[]) throws Exception{
     	String hostname = InetAddress.getLocalHost().getHostName();
     	Files.write(Paths.get("foo.out"), ("ENTERING " + hostname + "\n").getBytes(), StandardOpenOption.APPEND);
     	Thread.sleep(5000);
