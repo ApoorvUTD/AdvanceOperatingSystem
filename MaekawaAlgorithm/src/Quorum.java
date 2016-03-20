895572f94a0496610e7e9f11fef44c8dba77aa99
Apoorv Kumar <axk132231@utdallas.edu>
2016-03-20 13:22:07 -0500

    Added new quorum class which adds quorum request and has quorum properties Change-Id: I5541a839488b47f25ea98604a8ec05b33edf11c8

--- MaekawaAlgorithm/src/Quorum.java
+++ MaekawaAlgorithm/src/Quorum.java
@@ -2,6 +2,7 @@
 public class Quorum {
 	int nodeId;
 	MessageType requestStatus;
+	Node node;
 	
 	public Quorum(int nodeId, MessageType requestStatus) {
 		super();
@@ -19,6 +20,23 @@ public class Quorum {
 	}
 	public void setRequestStatus(MessageType requestStatus) {
 		this.requestStatus = requestStatus;
+	}
+	
+	
+	public synchronized void addNewQuorumRequest(int nodeId){
+		boolean isPresent=false;
+		int loop =0;
+		while (loop <node.quorumMemberRequest.size() ){
+			if (node.quorumMemberRequest.get(loop)[0] == node.nodeId){
+				isPresent=true;
+				break;
+			}
+			loop++;
+			}if(!isPresent){
+				int[] newMemebrRequest ={node.nodeId,Integer.parseInt(node.IPAddressMap.get(node.nodeId).totalMessagestoSend)};
+				node.quorumMemberRequest.add(newMemebrRequest);
+			}
+		
 	}
 
 }
